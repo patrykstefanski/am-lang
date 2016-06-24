30e6f3146f2c89e074ca900067c262767c517895
Patryk Stefanski <patryk.stefanski@protonmail.com>
2016-06-24 15:26:59 +0200

    new script for benchmarking

--- benchmarks/bench.sh
+++ benchmarks/bench.sh
@@ -3,42 +3,69 @@
 SRCDIR=$(dirname $0)
 TMPDIR=$(mktemp -d)
 
-run_am() {
-    echo "--------"
-    echo "$1 - am-lang"
-    time am-lang "$SRCDIR/$1.am"
+CC=clang
+TIME=/usr/bin/time # Use the time binary, not a shell builtin
+
+usage() {
+    echo "$0 [LANG] [TESTCASE] [NUM]"
+    echo
+    echo "Available languages: am-lang, c, java, java-int (for interpreted java), php, python"
+    echo "Available test cases: ackermann, fibonacci, prime"
+    echo "Example:"
+    echo "Run the ackermann test for am-lang 5 times"
+    echo "$0 am-lang ackermann 5"
+    exit 1
 }
 
-run_c() {
-    echo "--------"
-    echo "$1 - C"
-    gcc -O3 -Wall -Wextra "$SRCDIR/$1.c" -o "$TMPDIR/$1"
-    time "$TMPDIR/$1"
+if [[ ! " am-lang c java java-int php python " =~ " $1 " ]]; then
+    usage
+fi
+
+if [[ ! " ackermann fibonacci prime " =~ " $2 " ]]; then
+    usage
+fi
+
+if [[ -z "$3" ]]; then
+    usage
+fi
+
+exe() {
+    echo "$@"; "$@";
 }
 
-run_java() {
-    echo "--------"
-    echo "$1 - Java"
-    javac -d $TMPDIR "$SRCDIR/$1.java"
-    time java -cp "$TMPDIR" $1
-}
+case $1 in
+    "am-lang")
+        CMD="am-lang $2.am"
+        ;;
+    "c")
+        exe $CC -O3 -Wall -Wextra "$SRCDIR/$2.c" -o "$TMPDIR/$2"
+        CMD="$TMPDIR/$2"
+        ;;
+    "java")
+        exe javac -d $TMPDIR "$SRCDIR/$2.java"
+        CMD="java -cp $TMPDIR $2"
+        ;;
+    "java-int")
+        exe javac -d $TMPDIR "$SRCDIR/$2.java"
+        CMD="java -Xint -cp $TMPDIR $2"
+        ;;
+    "php")
+        CMD="php -f $SRCDIR/$2.php"
+        ;;
+    "python")
+        CMD="python $SRCDIR/$2.py"
+        ;;
+esac
 
-run_php() {
-    echo "--------"
-    echo "$1 - PHP"
-    time php -f "$SRCDIR/$1.php"
-}
-
-run_python() {
-    echo "--------"
-    echo "$1 - Python"
-    time python "$SRCDIR/$1.py"
-}
-
-for t in ackermann fibonacci prime; do
-    run_am $t
-    run_c $t
-    run_java $t
-    run_php $t
-    run_python $t
+total=0
+for i in $(seq 1 $3); do
+    t=$($TIME -p -f "%e" 2>&1 $CMD 1>/dev/null)
+    total=$(echo $total $t | awk '{ printf "%f", $1 + $2 }')
+    avg=$(echo $total $i | awk '{ printf "%.02f", $1 / $2 }')
+    if [[ -z "$min" ]] || [[ -z $(echo $t $min | awk '{ if ($1 > $2) print "dummy" }') ]]; then
+        min=$t
+    fi
+    echo "$i/$3 cur=${t}s avg=${avg}s min=${min}s"
 done;
+
+rm -fr $TMPDIR
